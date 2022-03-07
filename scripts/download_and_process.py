b8c5029540396b774c4e8dea11227b5eed8c903f
Tristan Lee <trislee@protonmail.com>
2022-03-06 20:57:47 -0600

    made log file append mode and added capability of not re-processing previously-processed URLs

--- scripts/download_and_process.py
+++ scripts/download_and_process.py
@@ -2,6 +2,7 @@
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 
+import os
 import json
 import logging
 
@@ -21,19 +22,36 @@ LOG_FILE = '../data/scraper.log'
 
 if __name__ == '__main__':
 
+  # Initialize logger in "append" mode
   logging.basicConfig(
     filename = LOG_FILE,
+    filemode = 'a',
     level = logging.INFO,
-    format='TIME: %(created)d | %(message)s')
+    format='TIME: %(created)f | %(message)s')
 
   logger = logging.getLogger(__name__)
 
+  # Read in list of URLs to process
   with open( URL_LIST_FILE, 'r' ) as f:
     url_list = list(filter(None, sorted(f.read( ).split( '\n' ))))
+
+  # If the output file contains previously-generated data, ignore
+  # previously-processed URLs
+  if os.path.isfile(OUTPUT_FILE):
+      with open(OUTPUT_FILE, 'r') as f:
+        lines = list(filter(None, f.read().split('\n')))
+      completed_urls = [json.loads(line)['url'] for line in lines]
+
+      url_list = list(set(url_list) - set(completed_urls))
+
+  # Loop over URLs, download and process data from page of each URL
+  #---------------------------------------------------------------------------#
 
   for i, url in enumerate(url_list):
 
     try:
+
+      # Make up to 5 attempts to retrieve content from a given URL
       n_retries = 0
       r = requests.get(url)
 
@@ -42,15 +60,22 @@ if __name__ == '__main__':
         r = requests.get(url)
 
       if r.status_code != 200:
+        # If all 5 attempts failed
         logger.info(f'URL#: {i} | URL: {url} | STATUS: failed | DETAIL: request failed')
+
       else:
+        # Content from the given URL was retrieved, so can be processed
         soup = BeautifulSoup(r.content, features = 'lxml')
         info = get_info(soup)
+
+        # Write JSON-serialized dict of all data from the video as a line in the
+        # output ndjson file
         with open(OUTPUT_FILE, 'a') as f:
           f.write(json.dumps(info) + '\n')
         logger.info(f'URL#: {i} | URL: {url} | STATUS: success | DETAIL: None')
 
     except Exception as e:
+      # Some error was encountered during the request for or parsing of content
       logger.info(f'URL#: {i} | URL: {url} | STATUS: failed | DETAIL: {e}')
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
